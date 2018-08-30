de4a4c4d4e9d2714fd051837687f6aa5ca00fd8e
MarekWesolowski <marek@tipser.com>
2018-08-30 14:59:47 +0200

    1. Fixed issue with running package on any other OS than Windows. 2. Upgraded to .NET 4.6.2 3. Upgraded nuget packages and added them to git ignore.

---
+++ Klarna.Kco/IISVersionProvider.cs
@@ -0,0 +1,48 @@
+﻿namespace Klarna.Checkout
+{
+    using Microsoft.Win32;
+    using System;
+    using System.Linq;
+
+    public class IISVersionProvider
+    {
+        public const string DefaultIISVersion = "Unknown_0.0";
+
+        private readonly PlatformID[] windowsPlatforms = new PlatformID[]
+        {
+            PlatformID.Win32NT,
+            PlatformID.Win32S,
+            PlatformID.Win32Windows,
+            PlatformID.WinCE,
+        };
+
+        /// <summary>
+        /// Gets IIS version from registry.
+        /// </summary>
+        /// <param name="os">Operating system information on which program is being run</param>
+        /// <returns>Returns string containing IIS version or Unknown_0.0 if it's not being run on any Windows OS</returns>
+        public string FindIISVersion(PlatformID platform)
+        {
+            if (windowsPlatforms.Contains(platform))
+            {
+                using (var iisKey = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\InetStp\"))
+                {
+                    if (iisKey != null)
+                    {
+                        var versionString = iisKey.GetValue("VersionString") as string;
+                        if (versionString != null)
+                        {
+                            var splitVersionString = versionString.Split(' ');
+                            if (splitVersionString.Length > 1)
+                            {
+                                return splitVersionString[1];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return DefaultIISVersion;
+        }
+    }
+}
